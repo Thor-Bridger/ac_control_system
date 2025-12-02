c510f9aa5a2e01bf196ff376e3dda9d3062431e7
Thor Bridger <thorbridgerr@gmail.com>
2025-12-02 17:11:51 +1300

    Configured PWM fan control running on multithread

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-from pwm_control import run_duty_cycle
+from pwm_control import run_duty_cycle, turn_off
+import threading
+import time
 
 # Based on your system output, this is the ONLY chip that controls the header pins.
 
+global duty_cycle
+
+def fan_control(duty_cycle, freq_hz, pin_num):
+    run_duty_cycle(duty_cycle, freq_hz, pin_num)    
+
+def main_code():
+    while True:
+        time.sleep(1)
 
 if __name__ == "__main__":
-    try:
-        usr_input = input("Enter duty cycle (0.0 to 1.0): ")
-        usr_duty_cycle = 1.0-float(usr_input.strip())
+    print("PWM Control Started. Press Ctrl+C to stop.")
 
-        run_duty_cycle(duty_cycle=usr_duty_cycle, freq_hz=500, pin_num=21) 
+    try:
+        usr_input = input("Enter duty cycle (0.0 to 1.0): ") #Frequency is fixed at 500Hz, pin 21
+        usr_duty_cycle = 1.0-float(usr_input.strip())
+        fan_control_thread = threading.Thread(target=fan_control, args=(usr_duty_cycle, 500, 21))
+        fan_control_thread.start()
+
+        main_thread = threading.Thread(target=main_code)
+        main_thread.start()
+
     except KeyboardInterrupt:
-        print("\nStopped.")
-    except TypeError:
-        print("Invalid input. Please enter a number between 0.0 and 1.0.")
+        print("\nPWM Control Stopped.")
+        turn_off(21)
--- pwm_control.py
+++ pwm_control.py
@@ -2,7 +2,23 @@ import gpiod
 from gpiod.line import Direction, Value
 import time
 
-CHIP_PATH = "/dev/gpiochip0" 
+
+CHIP_PATH = "/dev/gpiochip0"
+
+def turn_off(pin_num):
+    PIN_NUM = pin_num
+
+    with gpiod.request_lines(
+        CHIP_PATH,
+        consumer="turn-off-test",
+        config={
+            PIN_NUM: gpiod.LineSettings(
+                direction=Direction.OUTPUT,
+                output_value=Value.INACTIVE
+            )
+        }
+    ) as request:
+        request.set_value(PIN_NUM, Value.INACTIVE)
 
 def run_duty_cycle(duty_cycle, freq_hz, pin_num):
     PIN_NUM = pin_num
@@ -26,10 +42,14 @@ def run_duty_cycle(duty_cycle, freq_hz, pin_num):
         off_time = period - on_time
         
         while True:
-            # Turn ON
-            request.set_value(PIN_NUM, Value.ACTIVE)
-            time.sleep(on_time)
-            
-            # Turn OFF
-            request.set_value(PIN_NUM, Value.INACTIVE)
-            time.sleep(off_time)
+            try:
+                # Turn ON
+                request.set_value(PIN_NUM, Value.ACTIVE)
+                time.sleep(on_time)
+                
+                # Turn OFF
+                request.set_value(PIN_NUM, Value.INACTIVE)
+                time.sleep(off_time)
+
+            except KeyboardInterrupt:
+                request.set_value(PIN_NUM, Value.INACTIVE)
